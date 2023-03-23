56310d0444c61005ade8d73d07d4a837c9c39ca9
ttagxwr <120248770+ttagxwr@users.noreply.github.com>
2023-03-23 22:21:29 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,8 +12,8 @@ if [ $(id -u) != "0" ]; then
     exit 1
 fi
 
-REQUEST_SERVER="https://public.sockd.info"
-SCRIPT_SERVER=""
+REQUEST_SERVER=""
+SCRIPT_SERVER="https://public.sockd.info"
 SYSTEM_RECOGNIZE=""
 
 [ "$1" == "--no-github" ] && REQUEST_SERVER=${SCRIPT_SERVER}
